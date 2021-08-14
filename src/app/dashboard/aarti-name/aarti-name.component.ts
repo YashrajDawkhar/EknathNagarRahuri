f4ae1170894d41f14ce9e83f4096c5e2ac741561
Yashraj Dawkhar <yashrajdawkhar@gmail.com>
2021-08-14 19:38:18 +0530

    aarti component updated

--- src/app/dashboard/aarti-name/aarti-name.component.ts
+++ src/app/dashboard/aarti-name/aarti-name.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl ,FormGroup ,Validators}from '@angular/forms'
+import { BackendService } from '../backend.service';
+
 @Component({
   selector: 'app-aarti-name',
   templateUrl: './aarti-name.component.html',
   styleUrls: ['./aarti-name.component.css']
 })
 export class AartiNameComponent implements OnInit {
-  date7: any;
-  constructor() { }
+  
+  constructor(private service:BackendService ) { }
 
   ngOnInit(): void {
     
@@ -20,7 +22,12 @@ export class AartiNameComponent implements OnInit {
 
   artiDetails(){
     console.log(this.artiNames.value);
-    
+    this.service.postArtiName(this.artiNames.value).subscribe(data=>{
+      this.artiNames.reset()
+    },error=>{
+      console.log(error);
+      
+    })
   }
   
 }
--- src/app/dashboard/backend.service.ts
+++ src/app/dashboard/backend.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
+  url = "http://localhost:3000/artiName"
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  postArtiName(data:any){
+    return this.http.post(this.url,data)
+  }
+
+
 }
