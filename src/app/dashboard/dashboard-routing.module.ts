6cc3b1024878562ca2d8d2a88934e36e6cf4d4d2
Yashraj Dawkhar <yashrajdawkhar@gmail.com>
2021-08-17 22:44:54 +0530

    event update and meta tag

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -7,12 +7,13 @@ import { AartiNameComponent } from './aarti-name/aarti-name.component';
 import { DashboardComponent } from './dashboard.component';
 import { DonationEditComponent } from './donation-edit/donation-edit.component';
 import { DonationComponent } from './donation/donation.component';
+import { EventUpdatesComponent } from './event-updates/event-updates.component';
 
 const routes: Routes = [{ path: '', component: DashboardComponent ,canActivate:[LoginGuardGuard]},
 {path:'arti',component:AartiNameComponent ,canActivate:[LoginGuardGuard]},
 {path:'donation',component:DonationComponent ,canActivate:[LoginGuardGuard]},
-{path:'update/:id',component:DonationEditComponent ,canActivate:[LoginGuardGuard]}
-
+{path:'update/:id',component:DonationEditComponent ,canActivate:[LoginGuardGuard]},
+{path:'eventupdate',component:EventUpdatesComponent,canActivate:[LoginGuardGuard]}
 
 ];
 
